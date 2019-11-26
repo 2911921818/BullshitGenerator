d0c0807dc66c6c097942bae7a7b043c6321cf95a
devin <15071225115@163.com>
2019-11-26 10:27:04 +0800

    初始化 SpringBoot 项目

---
+++ src/main/java/com/devin/bullshitgenerator/BullshitGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.devin.bullshitgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullshitGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullshitGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devin/bullshitgenerator/BullshitGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devin.bullshitgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullshitGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
