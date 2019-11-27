fb6fe952db90821d33dbf02e70f4fe85ebfe4f05
devin <15071225115@163.com>
2019-11-27 17:17:30 +0800

    删除 thymeleaf ，因为用不到

--- src/main/java/com/devin/bullshitgenerator/controller/IndexController.java
+++ src/main/java/com/devin/bullshitgenerator/controller/IndexController.java
@@ -11,7 +11,7 @@ public class IndexController {
 
     @GetMapping("/index")
     public String index() {
-        return "index";
+        return "index.html";
     }
 
     @GetMapping("/generate")
